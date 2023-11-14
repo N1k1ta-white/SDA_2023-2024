0a9d29a73a942b029f4ef7a52d3f3afa14440d89
Nikita <nikitabeliy02@gmail.com>
2023-11-14 15:54:15 +0200

    Homework #6 is done

--- hw6/hw6_2.cpp
+++ hw6/hw6_2.cpp
@@ -1,95 +1,48 @@
-#include <unordered_map>
 #include <iostream>
-#include <vector>
-#include <climits>
-
 using namespace std;
 
 struct TreeNode {
-    TreeNode *left;
-    TreeNode *right;
-    int val;
-
-    TreeNode(int val, TreeNode *left, TreeNode *right) : left(left), right(right), val(val) {}
+    int ind;
+    long size = 1;
+    TreeNode* left;
+    TreeNode* right;
 };
+TreeNode nodes[10000000];
 
-struct TextNode {
-    int val;
-    int left;
-    int right;
-};
+long searcherOfSizes(TreeNode* root) {
+    if (root == nullptr)
+        return 0;
+    if (root->left == nullptr && root->right == nullptr)
+        return 1;
+    root->size += searcherOfSizes(root->left) + searcherOfSizes(root->right);
+    return root->size;
+}
 
-class Tree {
-    TreeNode *root;
-    int maxSum;
-
-    TreeNode *addNode(int i, const vector<TextNode> &arr) {
-        TreeNode *node = nullptr;
-        if (i == -1)
-            return node;
-        node = new TreeNode(arr[i].val, addNode(arr[i].left, arr),
-                            addNode(arr[i].right, arr));
-        return node;
+long findMaxRes(TreeNode* root, long n) {
+    long res = -1;
+    for (long i = 0; i < n; i++) {
+        long temp = (i == 0 ? 1 : n - nodes[i].size) *
+                (nodes[i].left == nullptr ? 1 : nodes[i].left->size) *
+                (nodes[i].right == nullptr ? 1 : nodes[i].right->size);
+        if (temp > res)
+            res = temp;
     }
-
-    void deleteTreeNode(TreeNode* node) {
-        if (node->right)
-            deleteTreeNode(node->right);
-        if (node->left)
-            deleteTreeNode(node->left);
-        delete node;
-    }
-
-    int getLongestPathInSubTree(TreeNode* sub) {
-        if (!sub)
-            return 0;
-        if (sub->val > maxSum)
-            maxSum = sub->val;
-        int left = getLongestPathInSubTree(sub->left),
-                right = getLongestPathInSubTree(sub->right);
-        if (left < sub->val && left < 0)
-            left = sub->val;
-        else
-            left += sub->val;
-        if (right < sub->val && right < 0)
-            right = sub->val;
-        else
-            right += sub->val;
-        int maxSubPath = max(max(left, right), left + right - sub->val);
-        if (maxSubPath > maxSum)
-            maxSum = maxSubPath;
-        return (left > right ? left : right);
-    }
-
-public:
-
-    ~Tree() {
-        deleteTreeNode(root);
-    }
-
-    Tree(const vector<TextNode> &arr) : maxSum(INT_MIN) {
-        root = addNode(0, arr);
-    }
-
-    Tree(const Tree& tr) = delete;
-    Tree operator=(const Tree& tr) = delete;
-
-    int findLongestPath() {
-        int path = getLongestPathInSubTree(root);
-        return path > maxSum ? path : maxSum;
-    }
-
-};
-
+    return res;
+}
 
 int main() {
-    int n;
+    long n, l, r, maxResult;
     cin >> n;
-    vector<TextNode> arr(n);
 
-    for (int i = 0; i < n; ++i)
-        cin >> arr[i].val >> arr[i].left >> arr[i].right;
+    for (long i = 0; i < n; i++) {
+        cin >> l >> r;
+        nodes[i].ind = i;
+        nodes[i].left = (l == -1 ? nullptr : &nodes[l]);
+        nodes[i].right = (r == -1 ? nullptr : &nodes[r]);
+    }
 
-    Tree tree(arr);
-    cout << tree.findLongestPath();
+    TreeNode* root = &nodes[0];
+    searcherOfSizes(root);
+    maxResult = findMaxRes(root, n);
+    cout << maxResult;
 }
